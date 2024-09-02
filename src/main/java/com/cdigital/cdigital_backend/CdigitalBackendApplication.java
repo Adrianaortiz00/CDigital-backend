468ac8dd3db98ae6773d9a6873c9cf175ac4c330
Adrianaortiz00 <a0207056@gmail.com>
2024-09-02 09:55:42 +0200

    Initial commit

---
+++ src/main/java/com/cdigital/cdigital_backend/CdigitalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cdigital.cdigital_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdigitalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdigitalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cdigital/cdigital_backend/CdigitalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdigital.cdigital_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdigitalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
